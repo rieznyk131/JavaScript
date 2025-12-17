8f5c1238ad5c191cf426c0c2e5c6b9db174982ff
rieznyk131 <m.rieznyk@gmail.com>
2025-12-17 22:34:50 +0100

    homework 2

--- Homework/Lesson_2/hw-2.13/main.js
+++ Homework/Lesson_2/hw-2.13/main.js
@@ -1,27 +1,34 @@
 let coursesAndDurationArray =
     [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
+        {title: 'JavaScript Complex', monthDuration: 5},
+        {title: 'Java Complex', monthDuration: 6},
+        {title: 'Python Complex', monthDuration: 6},
+        {title: 'QA Complex', monthDuration: 4},
+        {title: 'FullStack', monthDuration: 7},
+        {title: 'Frontend', monthDuration: 4}
     ]
+
+
 if (coursesAndDurationArray[0].monthDuration > 5) {
-    console.log(coursesAndDurationArray[0].title,'Super');
+    console.log(coursesAndDurationArray[0].title, '-','Super');
 }
+
 if (coursesAndDurationArray[1].monthDuration > 5) {
     console.log(coursesAndDurationArray[1].title,'-', 'Super');
 }
+
 if (coursesAndDurationArray[2].monthDuration > 5) {
     console.log(coursesAndDurationArray[2].title, '-', 'Super');
 }
+
 if (coursesAndDurationArray[3].monthDuration > 5) {
     console.log(coursesAndDurationArray[3].title, '-', 'Super');
 }
+
 if (coursesAndDurationArray[4].monthDuration > 5) {
     console.log(coursesAndDurationArray[4].title, '-', 'Super');
 }
+
 if (coursesAndDurationArray[5].monthDuration > 5) {
     console.log(coursesAndDurationArray[5].title, '-', 'Super');
 }
